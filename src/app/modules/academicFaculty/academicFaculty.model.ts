6facb284d76547239914f8a3d600bc1a8685397d
Raiyan <tajkierhaque@gmail.com>
2024-05-29 18:45:29 +0600

    academic faculty validation modified

--- src/app/modules/academicFaculty/academicFaculty.model.ts
+++ src/app/modules/academicFaculty/academicFaculty.model.ts
@@ -1,6 +1,4 @@
-import httpStatus from 'http-status';
 import { Schema, model } from 'mongoose';
-import ApiError from '../../errors/ApiError';
 import { TAcademicFaculty, TAcademicFacultyModel } from './academicFaculty.interface';
 
 const academicFacultySchema = new Schema<TAcademicFaculty, TAcademicFacultyModel>(
@@ -16,25 +14,11 @@ const academicFacultySchema = new Schema<TAcademicFaculty, TAcademicFacultyModel
 
 academicFacultySchema.static(
     'isAcademicFacultyAlreadyExists',
-    async function ({ name }: TAcademicFaculty) {
-        const isExists = await this.findOne({ name });
-
-        if (isExists) {
-            throw new ApiError(httpStatus.NOT_ACCEPTABLE, `${name} already exists!`);
-        }
+    async function (payload: Partial<TAcademicFaculty> | { _id: string }) {
+        const isExists = await this.findOne(payload);
+        return isExists;
     }
 );
-
-academicFacultySchema.pre('findOneAndUpdate', async function (next) {
-    const query = this.getQuery();
-
-    const isExists = await this.model.findOne(query);
-
-    if (!isExists) {
-        throw new ApiError(httpStatus.NOT_ACCEPTABLE, `Faculty does not exists!`);
-    }
-    next();
-});
 
 const AcademicFaculty = model<TAcademicFaculty, TAcademicFacultyModel>(
     'AcademicFaculty',
--- src/app/modules/academicFaculty/academicFaculty.service.ts
+++ src/app/modules/academicFaculty/academicFaculty.service.ts
@@ -1,8 +1,13 @@
+import httpStatus from 'http-status';
+import ApiError from '../../errors/ApiError';
 import { TAcademicFaculty } from './academicFaculty.interface';
 import AcademicFaculty from './academicFaculty.model';
 
 const createAcademicFacultyFromDB = async (payload: TAcademicFaculty) => {
-    await AcademicFaculty.isAcademicFacultyAlreadyExists(payload);
+    const isExists = await AcademicFaculty.isAcademicFacultyAlreadyExists({ name: payload.name });
+    if (isExists) {
+        throw new ApiError(httpStatus.NOT_ACCEPTABLE, `${payload.name} already exists!`);
+    }
     const result = await AcademicFaculty.create(payload);
     return result;
 };
@@ -12,12 +17,21 @@ const getAllAcademicFacultiesFromDB = async () => {
 };
 const getSingleAcademicFaultyFromDB = async (id: string) => {
     const result = await AcademicFaculty.findOne({ _id: id });
+    if (!result) {
+        throw new ApiError(httpStatus.NOT_FOUND, `Faculty does not exists!`);
+    }
     return result;
 };
 
 const updateAcademicFacultyToDB = async (id: string, payload: TAcademicFaculty) => {
-    await AcademicFaculty.isAcademicFacultyAlreadyExists(payload);
+    const isExists = await AcademicFaculty.isAcademicFacultyAlreadyExists({ name: payload.name });
+    if (isExists) {
+        throw new ApiError(httpStatus.NOT_ACCEPTABLE, `${payload.name} already exists!`);
+    }
     const result = await AcademicFaculty.findOneAndUpdate({ _id: id }, payload, { new: true });
+    if (!result) {
+        throw new ApiError(httpStatus.NOT_FOUND, `Faculty does not exists!`);
+    }
     return result;
 };
 
